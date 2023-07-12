52d300d6256b1d3beef1a15d8a096b53c2a97d0b
Victor <victoraranguren.dev@gmail.com>
2023-07-11 22:40:02 -0400

    Third Commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ console.log(process.env.DB_HOST);
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  port: "3306",
+  port: process.env.DB_PORT,
   dialect: "mysql",
   define: {
     timestamps: false,
